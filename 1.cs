da2e3264fdc127aa31008766b88edb84b11f2096
daniellau123 <101374421+daniellau123@users.noreply.github.com>
2022-04-01 11:27:19 +0800

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,14 @@
+int a=5;
+int b=7;
+if (a+b>15)
+{
+    Console.Writeline("The answer is greater than 15" );
+}
+else if (a+b> 15)
+{
+    Console.Writeline("The answer is between 15 and 10");
+}
+else
+{
+    Console.Writeline("The answer is not greater than 10");
+}
